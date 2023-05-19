f6fd4c5ce37e355ffdc955613777804e55fdca9f
thecatspyjamas <nealboylan@gmail.com>
2023-05-19 22:05:41 +0100

    Todo model class introduced

---
+++ app/models/Todo.java
@@ -0,0 +1,17 @@
+package models;
+
+import play.db.jpa.Model;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Todo extends Model
+{
+    public String title;
+
+    public Todo(String title)
+    {
+        this.title = title;
+    }
+}
+
